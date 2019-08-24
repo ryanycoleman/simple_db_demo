e351bbf80486e01b8b6b02e3a2eabdafcdce2e79
Bert Hajee <bert.hajee@enterprisemodules.com>
2019-08-24 17:34:26 +0200

    Update Vagrant file and add support for specifying puppet version

--- vm-scripts/install_puppet.sh
+++ vm-scripts/install_puppet.sh
@@ -1,7 +1,12 @@
 #
 # Install correct puppet version
 #
-echo 'Installing puppet agent'
-rpm -q puppet6-release || yum install -y --nogpgcheck https://yum.puppetlabs.com/puppet6/puppet6-release-el-7.noarch.rpm > /dev/null
-rpm -q puppet-agent || yum install -y --nogpgcheck puppet-agent
+if [ -f "/vagrant/puppet_version" ]; then
+  PACKAGE="puppet-agent-$(cat /vagrant/puppet_version)"
+else
+  PACKAGE="puppet-agent"
+fi
+echo "Installing $PACKAGE"
+yum install -y --nogpgcheck https://yum.puppetlabs.com/puppet6/puppet6-release-el-7.noarch.rpm > /dev/null
+yum install -y --nogpgcheck $PACKAGE
 rpm -q git || yum install -y --nogpgcheck git
